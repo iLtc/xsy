b76ef3ecc8e253be6a0b299c345f75ce8cb99131
iLtc <i@iltc.io>
2015-07-21 16:40:16 +0800

    ignore

--- Xsy/Runtime/Data/_fields/xsy.gonggao.php
+++
@@ -1,1 +0,0 @@
-<?php	return array ( 0 => 'Id', 1 => 'kaiguan', 2 => 'biaoti', 3 => 'neirong', '_autoinc' => true, '_pk' => 'Id', '_type' => array ( 'Id' => 'int(11)', 'kaiguan' => 'varchar(1)', 'biaoti' => 'varchar(255)', 'neirong' => 'varchar(255)', ), );?>
--- Xsy/Runtime/Data/_fields/xsy.info.php
+++
@@ -1,1 +0,0 @@
-<?php	return array ( 0 => 'id', 1 => 'name', 2 => 'class', '_autoinc' => false, '_pk' => 'id', '_type' => array ( 'id' => 'int(11)', 'name' => 'varchar(5)', 'class' => 'varchar(1)', ), );?>
